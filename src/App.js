af49866bd2f4705bcf7fd955a98dac095bb8082e
Oleg Berdyshev <berdyshevo@gmail.com>
2022-09-17 22:06:48 +0300

    refactor data.js & circle navigation (#5)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import './App.css';
 import {useState} from "react";
 import {IMAGE_HEIGHT} from "./config";
-
-const cards = [
-    {author: "unknownUser", img: 'https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?cs=srgb&dl=pexels-johann-1254140.jpg&fm=jpg'},
-    {author: "vitia", img: "https://images.pexels.com/photos/259772/pexels-photo-259772.jpeg?cs=srgb&dl=pexels-pixabay-259772.jpg&fm=jpg"},
-    {author: "coolGuy228", img: 'https://images.pexels.com/photos/1938351/pexels-photo-1938351.jpeg?cs=srgb&dl=pexels-tobias-bj%C3%B8rkli-1938351.jpg&fm=jpg'},
-    {author: "justAUser", img: 'https://images.pexels.com/photos/2446439/pexels-photo-2446439.jpeg?cs=srgb&dl=pexels-neha-pandey-2446439.jpg&fm=jpg'},
-]
+import {cards} from "./data";
 
 function App() {
     const [index, setIndex] = useState(0)
@@ -15,7 +9,7 @@ function App() {
     const onButtonClickNext = () => {
         setIndex((index) => {
             if (index === cards.length - 1) {
-                return index
+                return 0
             }
             return index + 1
         })
@@ -24,7 +18,7 @@ function App() {
     const onButtonClickPrev = () => {
         setIndex((index) => {
             if (index === 0) {
-                return index
+                return cards.length - 1
             }
             return index - 1})
     }
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const cards = [
+    {author: "unknownUser", img: 'https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?cs=srgb&dl=pexels-johann-1254140.jpg&fm=jpg'},
+    {author: "vitia", img: "https://images.pexels.com/photos/259772/pexels-photo-259772.jpeg?cs=srgb&dl=pexels-pixabay-259772.jpg&fm=jpg"},
+    {author: "coolGuy228", img: 'https://images.pexels.com/photos/1938351/pexels-photo-1938351.jpeg?cs=srgb&dl=pexels-tobias-bj%C3%B8rkli-1938351.jpg&fm=jpg'},
+    {author: "justAUser", img: 'https://images.pexels.com/photos/2446439/pexels-photo-2446439.jpeg?cs=srgb&dl=pexels-neha-pandey-2446439.jpg&fm=jpg'},
+]
