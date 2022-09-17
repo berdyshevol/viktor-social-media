b788cde141c9fa493a737bffe012094c81470fe7
Oleg Berdyshev <berdyshevo@gmail.com>
2022-09-17 16:25:10 +0300

    couter with a button (#1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(0)
+
+    const onButtonClick = () => {
+        setCount((count) => count + 1)
+    }
+
   return (
     <div className="App">
-      viktor
+      <div>{count}</div>
+        <div>
+            <button onClick={onButtonClick}>click for a surprise</button>
+        </div>
     </div>
   );
 }
