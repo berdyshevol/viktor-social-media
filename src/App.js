a6b34139430dad9224b98853fc1b2116996e634c
Oleg Berdyshev <berdyshevo@gmail.com>
2022-09-17 21:39:10 +0300

    added pictures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {useState} from "react";
+import {IMAGE_HEIGHT} from "./config";
 
 const cards = [
-    {author: "unknownUser"},
-    {author: "vitia"},
-    {author: "coolGuy228"},
-    {author: "justAUser"},
+    {author: "unknownUser", img: 'https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?cs=srgb&dl=pexels-johann-1254140.jpg&fm=jpg'},
+    {author: "vitia", img: "https://images.pexels.com/photos/259772/pexels-photo-259772.jpeg?cs=srgb&dl=pexels-pixabay-259772.jpg&fm=jpg"},
+    {author: "coolGuy228", img: 'https://images.pexels.com/photos/1938351/pexels-photo-1938351.jpeg?cs=srgb&dl=pexels-tobias-bj%C3%B8rkli-1938351.jpg&fm=jpg'},
+    {author: "justAUser", img: 'https://images.pexels.com/photos/2446439/pexels-photo-2446439.jpeg?cs=srgb&dl=pexels-neha-pandey-2446439.jpg&fm=jpg'},
 ]
 
 function App() {
@@ -28,11 +29,22 @@ function App() {
             return index - 1})
     }
 
-    const author = cards[index].author
+    const currentCard = cards[index]
+    const author = currentCard.author
+    const src = currentCard.img
+
 
   return (
     <div className="App">
-      <div>{author}</div>
+      <div>
+          {author}
+          <div className="image" style={{height: IMAGE_HEIGHT}}>
+            <img
+                src={src}
+                alt={author}
+            />
+          </div>
+      </div>
         <div>
             <button onClick={onButtonClickPrev}>prev</button>
             <button onClick={onButtonClickNext}>next</button>
