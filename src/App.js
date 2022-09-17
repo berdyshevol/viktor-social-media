b33ac2ab479c1266458800d752ac9648ba945203
Oleg Berdyshev <berdyshevo@gmail.com>
2022-09-17 17:09:08 +0300

    added cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import './App.css';
 import {useState} from "react";
 
+const cards = [
+    {author: "unknownUser"},
+    {author: "vitia"},
+    {author: "coolGuy228"},
+    {author: "justAUser"},
+]
+
 function App() {
-    const [count, setCount] = useState(0)
+    const [index, setIndex] = useState(0)
 
-    const onButtonClickIncrease = () => {
-        setCount((count) => count + 1)
+    const onButtonClickNext = () => {
+        setIndex((index) => {
+            if (index === cards.length - 1) {
+                return index
+            }
+            return index + 1
+        })
     }
 
-    const onButtonClickDecrease = () => {
-        setCount((count) => count - 1)
+    const onButtonClickPrev = () => {
+        setIndex((index) => {
+            if (index === 0) {
+                return index
+            }
+            return index - 1})
     }
+
+    const author = cards[index].author
 
   return (
     <div className="App">
-      <div>{count}</div>
+      <div>{author}</div>
         <div>
-            <button onClick={onButtonClickDecrease}>decrease</button>
-            <button onClick={onButtonClickIncrease}>increase</button>
+            <button onClick={onButtonClickPrev}>prev</button>
+            <button onClick={onButtonClickNext}>next</button>
         </div>
     </div>
   );
