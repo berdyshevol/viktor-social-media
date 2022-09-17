236804370f305a97bc36028ce5db49398b0f86a5
Oleg Berdyshev <berdyshevo@gmail.com>
2022-09-17 22:18:25 +0300

    added prettier

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import './App.css';
-import {useState} from "react";
-import {IMAGE_HEIGHT} from "./config";
-import {cards} from "./data";
+import "./App.css";
+import { useState } from "react";
+import { IMAGE_HEIGHT } from "./config";
+import { cards } from "./data";
 
 function App() {
-    const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(0);
 
-    const onButtonClickNext = () => {
-        setIndex((index) => {
-            if (index === cards.length - 1) {
-                return 0
-            }
-            return index + 1
-        })
-    }
+  const onButtonClickNext = () => {
+    setIndex((index) => {
+      if (index === cards.length - 1) {
+        return 0;
+      }
+      return index + 1;
+    });
+  };
 
-    const onButtonClickPrev = () => {
-        setIndex((index) => {
-            if (index === 0) {
-                return cards.length - 1
-            }
-            return index - 1})
-    }
+  const onButtonClickPrev = () => {
+    setIndex((index) => {
+      if (index === 0) {
+        return cards.length - 1;
+      }
+      return index - 1;
+    });
+  };
 
-    const currentCard = cards[index]
-    const author = currentCard.author
-    const src = currentCard.img
-
+  const currentCard = cards[index];
+  const author = currentCard.author;
+  const src = currentCard.img;
 
   return (
     <div className="App">
       <div>
-          {author}
-          <div className="image" style={{height: IMAGE_HEIGHT}}>
-            <img
-                src={src}
-                alt={author}
-            />
-          </div>
-      </div>
-        <div>
-            <button onClick={onButtonClickPrev}>prev</button>
-            <button onClick={onButtonClickNext}>next</button>
+        {author}
+        <div className="image" style={{ height: IMAGE_HEIGHT }}>
+          <img src={src} alt={author} />
         </div>
+      </div>
+      <div>
+        <button onClick={onButtonClickPrev}>prev</button>
+        <button onClick={onButtonClickNext}>next</button>
+      </div>
     </div>
   );
 }
